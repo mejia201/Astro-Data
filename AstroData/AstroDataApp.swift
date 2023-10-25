1317dc28e2766dbc62f8b8301a8d96713da132cf
mejia201 <bryan.mejia@catolica.edu.sv>
2023-10-25 11:33:10 -0600

    Version inicial

---
+++ AstroData/AstroDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroDataApp.swift
+//  AstroData
+//
+//  Created by Bryan Mejia on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AstroDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
